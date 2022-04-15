6609479cc92e3f42c4a7355c3a2d324b12d6e181
kfujita <kfujita@student.42tokyo.jp>
2022-04-16 01:22:53 +0900

    container nameを動的に変更できるように修正
    手元のM1 MacとGitHub Actions環境で名前の付け方(ハイフンとアンダーバー)が異なったため

--- local/localApiTestOnly.CI.sh
+++ local/localApiTestOnly.CI.sh
@@ -7,7 +7,8 @@
 # ===========================
 
 count=0
-while [ `docker logs local-mysql-1 2>&1 > /dev/null | grep "socket: '/var/run/mysqld/mysqld.sock'  port: 3306  MySQL Community Server - GPL." | wc -l` = 0 ]
+container_name=`docker ps --format "{{.Names}}" --filter "name=mysql"`
+while [ `docker logs $container_name 2>&1 > /dev/null | grep "socket: '/var/run/mysqld/mysqld.sock'  port: 3306  MySQL Community Server - GPL." | wc -l` = 0 ]
 do
   count=$((++count))
   if [ $count -gt 300 ] ; then
