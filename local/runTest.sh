39c181d99f0d019e2ad630d44d714c98e108d2af
kfujita <kfujita@student.42tokyo.jp>
2022-04-16 00:31:19 +0900

    ローカルで簡単にテストできるようにスクリプトを作成した

---
+++ local/runTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+cd $SCRIPT_DIR
+
+bash cpMysqlFile.sh
+
+docker-compose -f docker-compose-local-m1.yaml up -d
+
+cd ../scoring/tool/nodeTool
+npm ci
+
+cd $SCRIPT_DIR
+
+bash localApiTestOnly.CI.sh
+
+docker-compose -f docker-compose-local.yaml down
