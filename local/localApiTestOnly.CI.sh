439b52162810e869b3c118ccf2849369dd9b2427
kfujita <kfujita@student.42tokyo.jp>
2022-04-16 00:55:54 +0900

    タイムアウトを300秒に設定

--- local/localApiTestOnly.CI.sh
+++ local/localApiTestOnly.CI.sh
@@ -10,8 +10,8 @@ count=0
 while [ `docker logs local-mysql-1 2>&1 > /dev/null | grep "socket: '/var/run/mysqld/mysqld.sock'  port: 3306  MySQL Community Server - GPL." | wc -l` = 0 ]
 do
   count=$((++count))
-  if [ $count -gt 30 ] ; then
-    echo "Maybe failed to launch MySQL"
+  if [ $count -gt 300 ] ; then
+    echo "!!! Maybe failed to launch MySQL"
     exit 1
   fi
   echo "Waiting for MySQL to be ready... ($count seconds)"
