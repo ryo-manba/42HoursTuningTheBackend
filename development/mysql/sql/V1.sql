d21e0ed79b75acb5f459bd80bcfd487d0199b6a3
Ryomanba <datemanba3@gmail.com>
2022-04-16 22:30:42 +0900

    #11 fixes PRIMARY KEYとINDEXがかぶっているものを省略する

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,17 +1,7 @@
--- where、order byで指定されているもの全てにINDEXを貼っている
--- TODO: Primary key指定されている場合はいらないかも
-ALTER TABLE user               ADD INDEX user_user_id(user_id) USING HASH;
-ALTER TABLE group_info         ADD INDEX group_info_group_id(group_id) USING HASH;
-ALTER TABLE group_member       ADD INDEX group_member_user_id(user_id) USING HASH;
-ALTER TABLE record             ADD INDEX record_status(status) USING HASH,
-                               ADD INDEX record_record_id(record_id) USING HASH,
-                               ADD INDEX record_created_by(created_by) USING HASH,
-                               ADD INDEX recored_created_at(created_at) USING HASH,
-                               ADD INDEX recored_updated_at(updated_at) USING HASH;
-ALTER TABLE record_item_file   ADD INDEX record_item_file_linked_record_id(linked_record_id) USING HASH,
-                               ADD INDEX record_item_file_item_id(item_id) USING HASH;
-ALTER TABLE record_last_access ADD INDEX record_last_access(user_id) USING HASH;
-ALTER TABLE record_comment     ADD INDEX record_comment_linked_record_id(linked_record_id) USING HASH;
-ALTER TABLE category_group     ADD INDEX category_group_group_id(group_id) USING HASH;
-ALTER TABLE session            ADD INDEX session_value(value) USING HASH;
-ALTER TABLE file               ADD INDEX file_file_id(file_id) USING HASH;
+ALTER TABLE record             ADD INDEX record_status(status),
+                               ADD INDEX record_created_by(created_by),
+                               ADD INDEX recored_created_at(created_at),
+                               ADD INDEX recored_updated_at(updated_at);
+ALTER TABLE record_item_file   ADD INDEX record_item_file_linked_record_id(linked_record_id);
+ALTER TABLE record_comment     ADD INDEX record_comment_linked_record_id(linked_record_id);
+ALTER TABLE session            ADD INDEX session_value(value);
