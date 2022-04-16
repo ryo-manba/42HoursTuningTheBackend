ec5ce779c72f4819beca3a3a097fba8735852792
kfujita <kfujita@student.42tokyo.jp>
2022-04-16 22:04:49 +0900

    クエリをまとめて実行するように修正

--- development/backend/src/api/record-views.js
+++ development/backend/src/api/record-views.js
@@ -62,83 +62,69 @@ const tomeActive = async (req, res) => {
   const items = Array(recordResult.length);
   let count = 0;
 
-  const searchUserQs = 'select * from user where user_id = ?';
-  const searchGroupQs = 'select * from group_info where group_id = ?';
-  const searchThumbQs =
-    'select * from record_item_file where linked_record_id = ? order by item_id asc limit 1';
-  const countQs = 'select count(*) from record_comment where linked_record_id = ?';
-  const searchLastQs = 'select * from record_last_access where user_id = ? and record_id = ?';
+  const usersArr = await getValuesWhichHasValuesIn(
+    "user_id, name",
+    "user",
+    "user_id",
+    recordResult.map(v => v.created_by),
+  );
+  const groupInfoArr = await getValuesWhichHasValuesIn(
+    "group_id, name",
+    "group_info",
+    "group_id",
+    recordResult.map(v => v.application_group),
+  );
+  const thumbsArr = await getValuesWhichHasValuesIn(
+    "item_id, linked_record_id",
+    "record_item_file",
+    "linked_record_id",
+    recordResult.map(v => v.record_id),
+  );
+  const searchLastArr_recordId = await getValuesWhichHasValuesIn(
+    "user_id, record_id, access_time",
+    "record_last_access",
+    "record_id",
+    recordResult.map(v => v.record_id),
+  );
 
   for (let i = 0; i < recordResult.length; i++) {
-    const resObj = {
-      recordId: null,
-      title: '',
-      applicationGroup: null,
-      applicationGroupName: null,
-      createdBy: null,
-      createdByName: null,
-      createAt: '',
-      commentCount: 0,
-      isUnConfirmed: true,
-      thumbNailItemId: null,
-      updatedAt: '',
-    };
-
     const line = recordResult[i];
     mylog(line);
-    const recordId = recordResult[i].record_id;
-    const createdBy = line.created_by;
-    const applicationGroup = line.application_group;
-    const updatedAt = line.updated_at;
-    let createdByName = null;
-    let applicationGroupName = null;
-    let thumbNailItemId = null;
     let commentCount = 0;
     let isUnConfirmed = true;
 
-    const [userResult] = await pool.query(searchUserQs, [createdBy]);
-    if (userResult.length === 1) {
-      createdByName = userResult[0].name;
-    }
+    const createdByName = usersArr.find(v => v.user_id === line.created_by)?.name ?? null;
+    const applicationGroupName = groupInfoArr.find(v => v.group_id === line.application_group)?.name ?? null;
+    const thumbNailItemId = thumbsArr.find(v => v.linked_record_id === line.record_id)?.item_id ?? null;
+    const lastResult = searchLastArr_recordId.find(v => v.user_id === user.user_id && v.record_id === line.record_id)?.access_time;
 
-    const [groupResult] = await pool.query(searchGroupQs, [applicationGroup]);
-    if (groupResult.length === 1) {
-      applicationGroupName = groupResult[0].name;
-    }
-
-    const [itemResult] = await pool.query(searchThumbQs, [recordId]);
-    if (itemResult.length === 1) {
-      thumbNailItemId = itemResult[0].item_id;
-    }
-
-    const [countResult] = await pool.query(countQs, [recordId]);
+    const [countResult] = await pool.query('select count(*) from record_comment where linked_record_id = ?', [line.record_id]);
     if (countResult.length === 1) {
       commentCount = countResult[0]['count(*)'];
     }
 
-    const [lastResult] = await pool.query(searchLastQs, [user.user_id, recordId]);
-    if (lastResult.length === 1) {
-      mylog(updatedAt);
-      const updatedAtNum = Date.parse(updatedAt);
-      const accessTimeNum = Date.parse(lastResult[0].access_time);
+    if (lastResult !== undefined) {
+      mylog(line.updated_at);
+      const updatedAtNum = Date.parse(line.updated_at);
+      const accessTimeNum = Date.parse(lastResult);
       if (updatedAtNum <= accessTimeNum) {
         isUnConfirmed = false;
       }
     }
 
-    resObj.recordId = recordId;
-    resObj.title = line.title;
-    resObj.applicationGroup = applicationGroup;
-    resObj.applicationGroupName = applicationGroupName;
-    resObj.createdBy = createdBy;
-    resObj.createdByName = createdByName;
-    resObj.createAt = line.created_at;
-    resObj.commentCount = commentCount;
-    resObj.isUnConfirmed = isUnConfirmed;
-    resObj.thumbNailItemId = thumbNailItemId;
-    resObj.updatedAt = updatedAt;
-
-    items[i] = resObj;
+    items[i] = {
+      recordId: line.record_id,
+      title: line.title,
+      applicationGroup: line.application_group,
+      applicationGroupName: applicationGroupName,
+      createdBy: line.created_by,
+      createdByName: createdByName,
+      createAt: line.created_at,
+      commentCount: commentCount,
+      isUnConfirmed: isUnConfirmed,
+      thumbNailItemId: thumbNailItemId,
+      updatedAt: line.updated_at,
+    };
   }
 
   const [recordCountResult] = await pool.query(recordCountQs, param);
