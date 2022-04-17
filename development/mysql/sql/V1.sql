b4873d4e3d4b6c3861007fbfd0460eb55b99aa8a
Ryomanba <datemanba3@gmail.com>
2022-04-17 11:20:38 +0900

    #11 fixes 複合index

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -5,3 +5,7 @@ ALTER TABLE record             ADD INDEX record_status(status),
 ALTER TABLE record_item_file   ADD INDEX record_item_file_linked_record_id(linked_record_id);
 ALTER TABLE record_comment     ADD INDEX record_comment_linked_record_id(linked_record_id);
 ALTER TABLE session            ADD INDEX session_value(value);
+
+ALTER TABLE record ADD INDEX record_multi_1(category_id, application_group);
+ALTER TABLE record ADD INDEX record_multi_2(created_by, status);
+ALTER TABLE record_last_access  ADD INDEX record_last_multi(user_id, record_id);
